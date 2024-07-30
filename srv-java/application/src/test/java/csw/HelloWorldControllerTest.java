1c52925e71721d7abd37f2cf564a1cdaf5e75647
Gregor Wolf <gregor.wolf@gmail.com>
2024-07-30 14:55:27 +0200

    make tests work with authentication

--- srv-java/application/src/test/java/csw/HelloWorldControllerTest.java
+++ srv-java/application/src/test/java/csw/HelloWorldControllerTest.java
@@ -3,14 +3,14 @@ package csw;
 import static java.lang.Thread.currentThread;
 
 import com.sap.cloud.sdk.cloudplatform.thread.ThreadContextExecutor;
-// import com.sap.cloud.security.config.Service;
-// import com.sap.cloud.security.test.api.SecurityTestContext;
-// import com.sap.cloud.security.test.extension.SecurityTestExtension;
-// import com.sap.cloud.security.token.TokenClaims;
+import com.sap.cloud.security.config.Service;
+import com.sap.cloud.security.test.api.SecurityTestContext;
+import com.sap.cloud.security.test.extension.SecurityTestExtension;
+import com.sap.cloud.security.token.TokenClaims;
 import org.apache.commons.io.IOUtils;
-// import org.apache.http.HttpHeaders;
+import org.apache.http.HttpHeaders;
 import org.junit.jupiter.api.Test;
-// import org.junit.jupiter.api.extension.RegisterExtension;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -26,46 +26,40 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@TestPropertySource(
-        properties = {
+@TestPropertySource(properties = {
                 "sap.security.services.xsuaa.uaadomain=http://localhost:9001",
                 "sap.security.services.xsuaa.xsappname=xsapp!t0815",
-                "sap.security.services.xsuaa.clientid=sb-clientId!t0815" } )
-class HelloWorldControllerTest
-{
-    @Autowired
-    private MockMvc mvc;
+                "sap.security.services.xsuaa.clientid=sb-clientId!t0815" })
+class HelloWorldControllerTest {
+        @Autowired
+        private MockMvc mvc;
 
-    /*
-    @RegisterExtension
-    static SecurityTestExtension extension = SecurityTestExtension.forService(Service.XSUAA) // or Service.IAS
-            .setPort(9001);
+        @RegisterExtension
+        static SecurityTestExtension extension = SecurityTestExtension.forService(Service.XSUAA) // or Service.IAS
+                        .setPort(9001);
 
-    private String jwt;
-    */
+        private String jwt;
 
-    @Test
-    void test(/*SecurityTestContext context*/)
-    {
-        /*
-        jwt = context.getPreconfiguredJwtGenerator()
-                .withLocalScopes("Display")
-                .withClaimValue(TokenClaims.XSUAA.ORIGIN, "sap-default") // optional
-                .withClaimValue(TokenClaims.USER_NAME, "John") // optional
-                .createToken()
-                .getTokenValue();
-        jwt = "Bearer " + jwt;
-        */
+        @Test
+        void test(SecurityTestContext context) {
+                jwt = context.getPreconfiguredJwtGenerator()
+                                .withLocalScopes("Display")
+                                .withClaimValue(TokenClaims.XSUAA.ORIGIN, "sap-default") // optional
+                                .withClaimValue(TokenClaims.USER_NAME, "John") // optional
+                                .createToken()
+                                .getTokenValue();
+                jwt = "Bearer " + jwt;
 
-        final InputStream inputStream = currentThread().getContextClassLoader().getResourceAsStream("expected.json");
+                final InputStream inputStream = currentThread().getContextClassLoader()
+                                .getResourceAsStream("expected.json");
 
-        ThreadContextExecutor.fromNewContext().execute(() -> {
-            mvc
-                    .perform(MockMvcRequestBuilders.get("/hello")
-                            // .header(HttpHeaders.AUTHORIZATION, jwt)
-                    )
-                    .andExpect(status().isOk())
-                    .andExpect(content().json(IOUtils.toString(inputStream, StandardCharsets.UTF_8)));
-        });
-    }
+                ThreadContextExecutor.fromNewContext().execute(() -> {
+                        mvc
+                                        .perform(MockMvcRequestBuilders.get("/hello")
+                                                        .header(HttpHeaders.AUTHORIZATION, jwt))
+                                        .andExpect(status().isOk())
+                                        .andExpect(content()
+                                                        .json(IOUtils.toString(inputStream, StandardCharsets.UTF_8)));
+                });
+        }
 }
