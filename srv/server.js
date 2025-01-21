29a3a1382ce3b4df2f91bf9f3bde2bee2d0a81d5
Gregor Wolf <gregor.wolf@gmail.com>
2025-01-21 22:46:32 +0100

    Implement tenantIdFilterEventProcessing

--- srv/server.js
+++ srv/server.js
@@ -1,10 +1,34 @@
 const cds = require("@sap/cds");
 // https://cap.cloud.sap/docs/node.js/cds-log#configuring-log-levels
 const LOG = cds.log("mtxs-custom");
+const { config } = require("@cap-js-community/event-queue");
+
+// read from environment variable EVENT_PROCESSING_TENANTS
+const tenants = JSON.parse(process.env.EVENT_PROCESSING_TENANTS || "[]");
+LOG.info(`Tenants to be processed on this instance: ${tenants}`);
+async function checkIfTenantShouldBeProcessedOnInstance(tenantId) {
+  LOG.info(
+    `Checking if tenant ${tenantId} should be processed on this instance`
+  );
+  if (tenants.includes(tenantId)) {
+    LOG.info(`Tenant ${tenantId} should be processed on this instance`);
+    return true;
+  } else {
+    LOG.info(`Tenant ${tenantId} should not be processed on this instance`);
+    return false;
+  }
+}
+// Define a callback function to determine if a tenant's events should be processed
+config.tenantIdFilterEventProcessing = async (tenantId) => {
+  // Replace with your custom logic to decide whether to process the tenant
+  return await checkIfTenantShouldBeProcessedOnInstance(tenantId);
+};
+
 // const fesr = require("@sap/fesr-to-otel-js");
 
 // Read xsappname of services using xsenv
 const xsenv = require("@sap/xsenv");
+const e = require("express");
 xsenv.loadEnv();
 if (xsenv.readCFServices() === undefined) {
   LOG.info("starting locally");
